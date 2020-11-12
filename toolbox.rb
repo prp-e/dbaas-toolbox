4c48e2d2f403a58d3f18fe6f873efcae8ff52a40
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-12 17:42:26 +0330

    Modifying the damn thing to work properly

--- toolbox.rb
+++ toolbox.rb
@@ -1,7 +1,7 @@
 require 'socket' 
 
 def container_creation(name, port, root_password)
-    `docker run -p #{port}:3306 --name #{name}  -e MYSQL_ROOT_PASSWORD=#{root_password} -v $(pwd)/conf.d/my-custom.cnf:/etc/mysql/conf.d -d mysql/mysql-server`
+    `docker run -p #{port}:3306 --name #{name}  -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=#{root_password} -v $(pwd)/conf.d/my-custom.cnf:/etc/mysql/conf.d -d mysql/mysql-server`
 end 
 
 container_name = ARGV[0] 
