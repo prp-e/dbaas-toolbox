184327d8494f68753ea874fe5682bb4d36aecbb9
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-12 18:18:46 +0330

    Primary skeleton of the project is ready

--- toolbox.rb
+++ toolbox.rb
@@ -1,9 +1,10 @@
 require 'socket' 
 
-def container_creation(name, port, root_password)
-    `docker run -p #{port}:3306 --name #{name}  -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=#{root_password} -v $(pwd)/conf.d/my-custom.cnf:/etc/mysql/conf.d -d mysql/mysql-server`
+def container_creation(name, port, root_password, username, password)
+    `docker run -p #{port}:3306 --name #{name}  -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=#{root_password} -e MYSQL_USER=#{username} -e MYSQL_PASSWORD=#{password} -v $(pwd)/conf.d/my-custom.cnf:/etc/mysql/conf.d -d mysql/mysql-server`
     puts "Database container created successfully"
     `docker run -p 8000:80 --name #{name}-phpmyadmin -e PMA_PORTS=#{port} --link #{name}:db -d phpmyadmin/phpmyadmin`
+    puts "Now you can access your DB from the port 8000!"
 end 
 
 container_name = ARGV[0] 
