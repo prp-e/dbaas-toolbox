aa1b9a61744a275e0e2787379aa2ddc869219f72
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-13 06:08:21 +0330

    Fixed port double-checking

--- toolbox.rb
+++ toolbox.rb
@@ -9,12 +9,12 @@ def container_creation(name, username, password)
     if !port_blacklist.include?port 
         begin 
             socket = TCPSocket.new('localhost', port)
+            puts "#{port} is already taken"
+        rescue 
             `docker run -p #{port}:3306 --name #{name}  -e MYSQL_ROOT_HOST=%  -e MYSQL_DATABASE=#{name} -e MYSQL_USER=#{username} -e MYSQL_PASSWORD=#{password} -v $(pwd)/conf.d/my-custom.cnf:/etc/mysql/conf.d -d mysql/mysql-server`
             puts "Database container created successfully on #{port}. "
             `docker run -p #{port + 1}:80 --name #{name}-phpmyadmin -e PMA_PORTS=#{port} --link #{name}:db -d phpmyadmin/phpmyadmin`
             puts "Now you can access your DB from the port #{port + 1}!"
-        rescue 
-            puts "#{port} is already taken"
         end 
     else 
         puts "Try again later."
