5b4838b594cd0e789c905fcefb8c2cbf43144887
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-13 05:42:46 +0330

    Implementing the randomization

--- toolbox.rb
+++ toolbox.rb
@@ -1,19 +1,24 @@
 require 'socket' 
 
-def container_creation(name, port, root_password, username, password)
+def container_creation(name, root_password, username, password)
 
     port_blacklist = [3000, 3306, 4567, 8000, 8080]
 
-    `docker run -p #{port}:3306 --name #{name}  -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=#{root_password} -e MYSQL_USER=#{username} -e MYSQL_PASSWORD=#{password} -v $(pwd)/conf.d/my-custom.cnf:/etc/mysql/conf.d -d mysql/mysql-server`
-    puts "Database container created successfully"
-    `docker run -p 8000:80 --name #{name}-phpmyadmin -e PMA_PORTS=#{port} --link #{name}:db -d phpmyadmin/phpmyadmin`
-    puts "Now you can access your DB from the port 8000!"
+    port = Random.rand(2000...65000)
+    
+    if port_blacklist.include?port 
+        `docker run -p #{port}:3306 --name #{name}  -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=#{root_password} -e MYSQL_USER=#{username} -e MYSQL_PASSWORD=#{password} -v $(pwd)/conf.d/my-custom.cnf:/etc/mysql/conf.d -d mysql/mysql-server`
+        puts "Database container created successfully on #{port}. "
+        `docker run -p #{port + 1}:80 --name #{name}-phpmyadmin -e PMA_PORTS=#{port} --link #{name}:db -d phpmyadmin/phpmyadmin`
+        puts "Now you can access your DB from the port #{port + 1}!"
+    else 
+        puts "Try again later."
+    end 
 end 
 
-container_name = ARGV[0] 
-container_port = ARGV[1] 
-container_root_password = ARGV[2]
-container_user_name = ARGV[3] 
-container_password = ARGV[4]
+container_name = ARGV[0]  
+container_root_password = ARGV[1]
+container_user_name = ARGV[2] 
+container_password = ARGV[3]
 
-container_creation(container_name, container_port, container_root_password, container_user_name, container_password)
+container_creation(container_name, container_root_password, container_user_name, container_password)
